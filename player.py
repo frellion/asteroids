075f3b0a0c2a259a8b0f07c453a52b640f8e3340
frellion <melissa.heechung@gmail.com>
2025-03-26 19:46:41 -0400

    shot rate restricted; no collisions

--- player.py
+++ player.py
@@ -5,12 +5,14 @@ from constants import PLAYER_RADIUS
 from constants import PLAYER_TURN_SPEED
 from constants import PLAYER_SPEED
 from constants import PLAYER_SHOOT_SPEED
+from constants import PLAYER_SHOOT_COOLDOWN
 
 class Player(CircleShape):
 
 	def __init__(self, x, y):
 		super().__init__(x, y, PLAYER_RADIUS)
 		self.rotation = 0
+		self.cooldown_timer = 0
 
 	# in the player class
 	def triangle(self):
@@ -29,6 +31,7 @@ class Player(CircleShape):
 
 	def update(self, dt):
 		keys = pygame.key.get_pressed()
+		self.cooldown_timer -= dt
 
 		if keys[pygame.K_a]:
 			self.rotate(dt * -1)
@@ -39,7 +42,8 @@ class Player(CircleShape):
 		if keys[pygame.K_s]:
 			self.move(dt * -1)
 		if keys[pygame.K_SPACE]:
-			self.shoot()
+			if not (self.cooldown_timer>0):
+				self.shoot()
 
 	def move(self, dt):
 		forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -48,3 +52,4 @@ class Player(CircleShape):
 	def shoot(self):
 		shot = Shot(self.position.x, self.position.y)
 		shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+		self.cooldown_timer = PLAYER_SHOOT_COOLDOWN
